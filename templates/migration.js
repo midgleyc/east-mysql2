fb9a0f6d057f3d49c0674409bc7ab26c8b263652
Chris Midgley <chris.midgley@dunecomputers.co.uk>
2021-01-15 13:56:57 +0000

    fix: don't advocate closing connections in migrations
    We need the connection when it comes to marking the migration as
    executed.

--- templates/migration.js
+++ templates/migration.js
@@ -1,17 +1,9 @@
 export const tags = []
 
 export const migrate = async (client) => {
-  try {
-    // await client.db.query("SQL")
-  } finally {
-    await client.db.end()
-  }
+  // await client.db.query("SQL")
 }
 
 export const rollback = async (client) => {
-  try {
-    // await client.db.query("SQL")
-  } finally {
-    await client.db.end()
-  }
+  // await client.db.query("SQL")
 }
--- templates/migration.ts
+++ templates/migration.ts
@@ -3,17 +3,9 @@ import type {MySQLAdapterDb} from 'east-mysql2'
 export const tags = []
 
 export const migrate = async (client: MySQLAdapterDb): Promise<void> => {
-  try {
-    // await client.db.query("SQL")
-  } finally {
-    await client.end()
-  }
+  // await client.db.query("SQL")
 }
 
 export const rollback = async (client: MySQLAdapterDb): Promise<void> => {
-  try {
-    // await client.db.query("SQL")
-  } finally {
-    await client.end()
-  }
+  // await client.db.query("SQL")
 }
