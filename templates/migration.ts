cb3a9ae972f89af536360508159c328371186a41
Chris Midgley <chris.midgley@dunecomputers.co.uk>
2021-01-16 21:49:24 +0000

    chore: in ts template, note tags are strings

--- templates/migration.ts
+++ templates/migration.ts
@@ -1,6 +1,6 @@
 import type {MySQLClient} from 'east-mysql2'
 
-export const tags = []
+export const tags: string[] = []
 
 export const migrate = async (client: MySQLClient): Promise<void> => {
   // await client.db.query("SQL")
